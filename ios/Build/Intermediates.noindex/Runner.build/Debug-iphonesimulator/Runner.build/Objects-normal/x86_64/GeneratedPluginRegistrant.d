80062999279478a6642c708f37f20f9093d55635
Flavio Silverio <>
2021-01-02 15:25:36 +0000

    Adds categories and category screen

---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,21 @@
+dependencies: \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.3.sdk/usr/include/mach-o/dyld.modulemap \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Flutter/Flutter.framework/Headers/Flutter.h
---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/flavio.silverio/Desktop/flutter/recipes_app/recipes/ios/Build/Intermediates.noindex/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
